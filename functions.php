8e3ded93a21295b54bb0a70bed0056d2b589e8d9
Shkoterman <chkoterman@gmail.com>
2024-08-29 00:59:11 +0200

    ooo

--- functions.php
+++ functions.php
@@ -231,11 +231,11 @@ function display_events_calendar() {
 	$events = get_airtable_events();
 	
 	$predata = $events['body'];
-	#$data = json_decode($predata);
+	$data = json_decode($predata, true);
 	
-	echo $predata;
+	echo $data;
 
-    echo gettype($predata);
+    echo gettype($data);
 	
 
     
