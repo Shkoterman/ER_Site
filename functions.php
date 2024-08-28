8de48aa484bda72898dd23d3b66d46824987bd75
Shkoterman <chkoterman@gmail.com>
2024-08-29 00:04:28 +0200

    123

--- functions.php
+++ functions.php
@@ -210,7 +210,7 @@ add_action( 'init', 'twentytwentyfour_pattern_categories' );
 function get_airtable_events() {
 
 
-    $api_url = 'https://api.airtable.com/app7g2ANnagHYZkZ8/tbleWW3ENwjP0uDgh/viww3iBpMsoPjyCpq';
+    $api_url = 'https://api.airtable.com/v0/app7g2ANnagHYZkZ8/tbleWW3ENwjP0uDgh';
 
     // Заголовки для авторизации и формата запроса
     $headers = array(
@@ -223,16 +223,13 @@ function get_airtable_events() {
         'headers' => $headers
     ));
     
-
-    $data1 = 'asdasd';
-
     return $response;
 }
 
 function display_events_calendar() {
 	$events = get_airtable_events();
 
-    echo var_dump($events['body']);
+    echo gettype($events['body']);
 }
 
 add_shortcode('airtable_events', 'display_events_calendar');
